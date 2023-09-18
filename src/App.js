aab0e990e0db38eee4b1c367230cb6c4b3d6066a
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2023-09-19 00:06:06 +0530

    publishing version 1.0.0 of chat app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,144 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import Message from './Components/Message';
+import { app } from './firebase';
+import {
+  Container,
+  Box,
+  VStack,
+  Button,
+  Input,
+  HStack
+} from '@chakra-ui/react';
+import {
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut
+} from 'firebase/auth';
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy
+} from 'firebase/firestore';
 
-function App() {
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const loginHandler = () => {
+  const provider = new GoogleAuthProvider();
+
+  signInWithPopup(auth, provider);
+};
+
+const logoutHandler = () => signOut(auth);
+
+//Functional Component
+const App = () => {
+  const [user, setUser] = useState(false);
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const divForScroll = useRef(null);
+
+  //creating a submit Handler
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      setMessage('');
+
+      await addDoc(collection(db, 'Messages'), {
+        text: message,
+        uid: user.uid,
+        uri: user.photoURL,
+        createdAt: serverTimestamp()
+      });
+
+      divForScroll.current.scrollIntoView({ behavior: 'smooth' });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    const q = query(collection(db, 'Messages'), orderBy('createdAt', 'asc'));
+
+    const unsubscribe = onAuthStateChanged(auth, (data) => {
+      setUser(data);
+    });
+
+    const unsubscribeForMessage = onSnapshot(q, (snap) => {
+      setMessages(
+        snap.docs.map((item) => {
+          const id = item.id;
+          return { id, ...item.data() };
+        })
+      );
+    });
+
+    return () => {
+      unsubscribe();
+      unsubscribeForMessage();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box bg={'red.50'}>
+      {user ? (
+        <Container bg={'white'} h={'100vh'}>
+          <VStack h={'full'} paddingY={'4'}>
+            <Button onClick={logoutHandler} w={'full'} colorScheme={'red'}>
+              logout
+            </Button>
+
+            <VStack
+              h={'full'}
+              w={'full'}
+              overflowY={'auto'}
+              css={{
+                '&::-webkit-scrollbar': {
+                  display: 'none'
+                }
+              }}
+            >
+              {messages.map((item) => (
+                <Message
+                  key={item.id}
+                  user={item.uid === user.uid ? 'me' : 'other'}
+                  text={item.text}
+                  uri={item.uri}
+                />
+              ))}
+              <div ref={divForScroll}></div>
+            </VStack>
+
+            <form onSubmit={submitHandler} style={{ width: '100%' }}>
+              <HStack>
+                <Input
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  placeholder="Type your Message..."
+                />
+                <Button colorScheme={'purple'} type="submit">
+                  Send
+                </Button>
+              </HStack>
+            </form>
+          </VStack>
+        </Container>
+      ) : (
+        <VStack h={'100vh'} bg={'white'} justifyContent={'center'}>
+          <Button colorScheme="purple" onClick={loginHandler}>
+            Login with Google
+          </Button>
+        </VStack>
+      )}
+    </Box>
   );
-}
+};
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDXBZtZ_wDLuUY0BQhueIqbGYuGAtUHSf0',
+  authDomain: 'chatapp-22274.firebaseapp.com',
+  projectId: 'chatapp-22274',
+  storageBucket: 'chatapp-22274.appspot.com',
+  messagingSenderId: '229610490632',
+  appId: '1:229610490632:web:ce5c66429fa974a034babd'
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
